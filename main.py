21c5e8d892e50523457619816eb7f807afc2ef70
Guangyang Li <mail@guangyangli.com>
2015-08-23 15:18:01 -0400

    Add comments

--- main.py
+++ main.py
@@ -17,22 +17,31 @@ EMOJI_FOLDER = 'emoji'
 
 
 def avg_color(colors):
-    # Given a list of pixels, return the average color of them
+    """
+    Calculate average color
+    :param colors: a list of RGB colors
+    :return: a single tuple of RGB color
+    """
     return tuple(pixel / len(colors) for pixel in reduce(lambda p, q: map(lambda x, y: float(x)+y, p, q), colors))
 
 
-def zoom_image(radio=DEFAULT_EMOJI_SIZE):
+def zoom_image(ratio=DEFAULT_EMOJI_SIZE):
+    """
+    :param ratio: the size of a mosaic block
+    """
     width, height = image.size
-    zoomed_pixels = np.zeros((3, width/radio, height/radio))
 
-    width_limit = width - radio if width % radio != 0 else width
-    height_limit = height - radio if height % radio != 0 else height
-    for x, y in product(range(0, width_limit, radio), range(0, height_limit, radio)):
-        ran = range(radio)
+    # Initialize a zere RGB matrix with zoomed size
+    zoomed_image = np.zeros((3, width/ratio, height/ratio))
+
+    # For each block, take the averge color and fill it into the zoomed image matrix
+    for x, y in product(range(0, width - ratio if width % ratio != 0 else width, ratio),
+                        range(0, height - ratio if height % ratio != 0 else height, ratio)):
+        ran = range(ratio)
         color = avg_color([img[loc] for loc in map(lambda p: (x+p[0], y+p[1]), product(ran, ran)) if loc[0] < width and loc[1] < height])
         for rgb in range(3):
-            zoomed_pixels[rgb][x/radio, y/radio] = color[rgb]
-    return zoomed_pixels
+            zoomed_image[rgb][x/ratio, y/ratio] = color[rgb]
+    return zoomed_image
 
 
 def zoom_emoji(file=None, size=DEFAULT_EMOJI_SIZE):
