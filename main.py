f67d43862df2867ca73850f1dd7f5302e01e7c4f
Guangyang Li <gli@crowdskout.com>
2015-08-22 23:40:46 -0400

    add average color function

--- main.py
+++ main.py
@@ -4,28 +4,31 @@
 import json
 from PIL import Image
 import numpy as np
+from itertools import product
 
 filename = 'lena512color.tiff'
 
 image = Image.open(filename)
 len([p for p in image.getdata()])
 
-i = image.load()
+img = image.load()
 
 width, height = image.size
 
 
 def avg_color(color_list):
     # Given a list of pixels, return the average color of them
-    return tuple(pixel / len(color_list) for pixel in reduce(lambda p, q: map(lambda x, y: x + y, p, q), color_list))
-
-
-zoomed_pixels = np.zeros((3, width/2, height/2))
+    return tuple(pixel / len(color_list) for pixel in reduce(lambda p, q: map(lambda x, y: float(x) + float(y), p, q), color_list))
 
 
 def zoom_pic(radio=2):
-    for x in range(0, width, 2):
-        for y in range(0, height, 2):
-            color = avg_color((i[x, y], i[x+1, y], i[x, y+1], i[x+1, y+1]))
-            for i in range(3):
-                zoomed_pixels[i][x/2, y/2] = color[i]
+    zoomed_pixels = np.zeros((3, width/radio, height/radio))
+    for x in range(0, width, radio):
+        for y in range(0, height, radio):
+            # TODO: 不整除的情况
+            color = avg_color([img[location] for location in map(lambda p: (x+p[0], y+p[1]), product(range(radio), range(radio)))])
+            for rgb in range(3):
+                zoomed_pixels[rgb][x/radio, y/radio] = color[rgb]
+    return zoomed_pixels
+
+zoom_pic(4)
